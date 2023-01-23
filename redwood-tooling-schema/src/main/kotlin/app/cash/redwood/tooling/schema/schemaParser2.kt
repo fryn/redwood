23e7a05269ef769e222ef5bb23a560e7fb7b97dd
Jake Wharton <jw@squareup.com>
2023-01-23 08:55:29 -0500

    WIP

--- redwood-tooling-schema/src/main/kotlin/app/cash/redwood/tooling/schema/schemaParser2.kt
+++ redwood-tooling-schema/src/main/kotlin/app/cash/redwood/tooling/schema/schemaParser2.kt
@@ -15,25 +15,32 @@
  */
 package app.cash.redwood.tooling.schema
 
-import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY
+import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSourceLocation
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.cli.jvm.compiler.NoScopeRecordCliBindingTrace
+import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
+import org.jetbrains.kotlin.codegen.kotlinType
 import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.com.intellij.psi.PsiManager
+import org.jetbrains.kotlin.config.CommonConfigurationKeys.MODULE_NAME
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.resolve.lazy.declarations.FileBasedDeclarationProviderFactory
 
-public fun parseSchema(files: Collection<VirtualFile>): Schema {
-  val configuration = CompilerConfiguration()
-  configuration.put(
-    CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, object : MessageCollector {
+public fun parseSchema(
+  files: Collection<VirtualFile>,
+): Schema {
+  val messageCollector = object : MessageCollector {
     override fun clear() = Unit
     override fun hasErrors() = false
 
@@ -44,18 +51,45 @@ public fun parseSchema(files: Collection<VirtualFile>): Schema {
     ) {
       println("$severity: $message")
     }
-  })
+  }
 
-  val project = KotlinCoreEnvironment.createForProduction(
+  val configuration = CompilerConfiguration()
+  configuration.put(MODULE_NAME, "schema")
+  configuration.put(MESSAGE_COLLECTOR_KEY, messageCollector)
+
+  val environment = KotlinCoreEnvironment.createForProduction(
     Disposer.newDisposable(),
     configuration,
     EnvironmentConfigFiles.JVM_CONFIG_FILES,
-  ).project
+  )
+  val project = environment.project
+
+  val analyzer = AnalyzerWithCompilerReport(
+    messageCollector,
+    environment.configuration.languageVersionSettings,
+    false,
+  )
 
   val psi = PsiManager.getInstance(project)
+  // TODO do something about files which are not KtFile.
+  val ktFiles = files.mapNotNull { psi.findFile(it) as? KtFile }
+
+  analyzer.analyzeAndReport(ktFiles) {
+    TopDownAnalyzerFacadeForJVM.analyzeFilesWithJavaIntegration(
+      environment.project,
+      ktFiles,
+      NoScopeRecordCliBindingTrace(),
+      environment.configuration,
+      environment::createPackagePartProvider,
+      ::FileBasedDeclarationProviderFactory
+    )
+  }
+
+  val bindingContext = analyzer.analysisResult.bindingContext
+
   val types = buildMap {
-    files.forEach { file ->
-      for (child in (psi.findFile(file) as KtFile).recursiveChildren) {
+    ktFiles.forEach { file ->
+      for (child in file.recursiveChildren) {
         if (child is KtClassOrObject) {
           put(child.fqName, child)
         }
@@ -63,12 +97,12 @@ public fun parseSchema(files: Collection<VirtualFile>): Schema {
     }
   }
 
-
   fun printWidget(widget: KtClassOrObject) {
     println("CLASS: ${widget.fqName}")
     println("  COMMENT ${widget.docComment?.text?.replace("\n", "\\n")}")
     widget.primaryConstructorParameters.forEach {
       println("  TRAIT: ${it.name}")
+      println("    TYPE: ${it.kotlinType(bindingContext)}")
       println("    COMMENT: ${it.docComment?.text?.replace("\n", "\\n")}")
       println("    ANNOTATIONS: ${it.annotationEntries}")
       println("    DEFAULT: ${it.defaultValue?.text}")
